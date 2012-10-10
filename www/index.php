f81561a9084f2f524171a661fee15db6edce4f44
quantum13 <quant13@gmail.com>
2012-10-10 22:37:33 +1100

    changing directore structure
    setting up public directory

---
+++ www/index.php
@@ -0,0 +1,36 @@
+<?php
+namespace System;
+
+class Router {
+
+    public function match($route, $callback) {
+        if($route==parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)) {
+
+            $request = array(
+                'GET' => $_GET,
+                'POST' => $_POST,
+                'COOKIE' => $_COOKIE
+            );
+
+            $callback($request);
+        }
+
+    }
+}
+
+//Twig
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig_Loader_Filesystem('templates');
+$twig = new \Twig_Environment($loader, array(
+    'cache' => 'cache',
+));
+//end of twig
+
+$router = new Router();
+
+$router->match('/url', function($request) use ($twig) {
+    //var_dump($request);
+    print $twig->render('base.html', array('var' => 'sd'));
+});
+
