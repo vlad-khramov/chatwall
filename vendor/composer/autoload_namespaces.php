df992f87a093a18b38df6679ee7d61467f043df7
quantum13 <quant13@gmail.com>
2012-10-10 22:53:00 +1100

    installing doctrine orm via composer

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,4 +7,8 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Twig_' => $vendorDir . '/twig/twig/lib/',
+    'Symfony\\Component\\Console' => $vendorDir . '/symfony/console/',
+    'Doctrine\\ORM' => $vendorDir . '/doctrine/orm/lib/',
+    'Doctrine\\DBAL' => $vendorDir . '/doctrine/dbal/lib/',
+    'Doctrine\\Common' => $vendorDir . '/doctrine/common/lib/',
 );
--- www/index.php
+++ www/index.php
@@ -19,7 +19,7 @@ class Router {
 }
 
 //Twig
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 $loader = new \Twig_Loader_Filesystem('templates');
 $twig = new \Twig_Environment($loader, array(
