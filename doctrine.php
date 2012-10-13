d92ed4d843da3ef13714271080816b6b79d68d6b
quantum13 <quant13@gmail.com>
2012-10-14 10:29:32 +1100

    dir fix

--- doctrine.php
+++ doctrine.php
@@ -7,7 +7,7 @@ use \Symfony\Component\Console\Helper\HelperSet;
 require_once 'vendor/autoload.php';
 require_once 'app/autoload.php';
 
-define('SYSTEM_DIR', dirname(__DIR__));
+define('SYSTEM_DIR', __DIR__);
 define('DEBUG', false);
 
 $em = \App\System\Locator::getEm();
