06e04609f04017758cced72e562493f5affe8033
quantum13 <quant13@gmail.com>
2012-10-14 10:25:55 +1100

    env settings

--- www/index.php
+++ www/index.php
@@ -3,8 +3,10 @@
 require_once '../vendor/autoload.php';
 require_once '../app/autoload.php';
 
+defined('DEBUG')
+    || define('DEBUG', (getenv('DEBUG') ? getenv('DEBUG') : 0));
+
 define('SYSTEM_DIR', dirname(__DIR__));
-define('DEBUG', true);
 
 $app = new App\System\App();
 $app->run();
