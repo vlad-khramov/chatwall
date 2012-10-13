8679377eb3a7542ffa7d9e5d3d6c0209749db151
quantum13 <quant13@gmail.com>
2012-10-13 11:25:03 +1100

    db options, base routing

--- app/config.php
+++ app/config.php
@@ -2,8 +2,17 @@
 
 return array(
     'routes' => array(
-        '/url' => 'home.home'
+        '/' => 'home.home',
+        '/user/save' => 'home.user_save',
     ),
     'cache_dir' => SYSTEM_DIR . '/cache',
     'templates_dir' => SYSTEM_DIR . '/templates',
+    'db' => array(
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'user' => 'username',
+        'password' => 'password',
+        'dbname' => 'chatwall',
+
+    )
 );
